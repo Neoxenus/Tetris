6bf914442fd76b0c49a5c70f2eb103284eb46f58
Volodymyr Kyryliuk <91565516+Neoxenus@users.noreply.github.com>
2022-02-24 20:32:42 +0200

    Narrowing of methods scope

--- Tetris/Cell.h
+++ Tetris/Cell.h
@@ -7,9 +7,11 @@ class Cell
 public:
 	Cell();
 	~Cell();
-	sf::Color GetColorRGB(int TypeOfColor);
+	
 	void setCell(int posX, int posY, int offsetX, int offsetY, int color);
 	void draw(sf::RenderWindow &window);
 private:
+	sf::Color GetColorRGB(int TypeOfColor);
+
 	int posX, posY, offsetX, offsetY, color;
 };
